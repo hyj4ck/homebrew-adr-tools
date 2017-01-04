749b82ffd299bc162f738f551c0da8cf8a3c93b2
Radek Hajek <hyj4ck@gmail.com>
2017-01-04 15:22:14 +0100

    update adr version to 1.2

--- adr_tools.rb
+++ adr_tools.rb
@@ -6,8 +6,6 @@ class AdrTools < Formula
   sha256 "3f45646c099ae996b97c855a9a27ed540e076580eba59c2e8613453b7a67e412"
 
   def install
-    inreplace 'src/adr-init', '$(dirname $0)', prefix
-    inreplace 'src/adr-new', 'ADR_TEMPLATE:-$(dirname $0)/template.md', "ADR_TEMPLATE:-#{prefix}/template.md"
 
     prefix.install Dir["src/*.md"]
     bin.install Dir["src/*"] - Dir["src/*.md"]
